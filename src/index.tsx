a91340429899c7b2518eb86816f19b68c002aa9e
DaWenXi <zengxin@bianfeng.com>
2022-09-02 13:31:24 +0800

    feat: format

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.scss'
+import './styles/index.scss';
 
 const num: number = 111;
 
-console.log('num',   num);
+console.log('num', num);
 
 const App = () => {
-  return (
-    <div>
-      <h1>Hello!!</h1>
-      <h2>Welcome to your First React App..!</h2>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Hello!!</h1>
+			<h2>Welcome to your First React App..!</h2>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
